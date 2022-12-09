15e2266c0c41c87e07b8906444b17c7707da6966
Pamod460 <pamodkaushika@gmail.com>
2022-12-09 20:32:42 +0530

    Employee UI created

--- Sprint1 (employee)/S1V1 ViewAll/Test.java
+++ Sprint1 (employee)/S1V1 ViewAll/Test.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.List;
 
 public class Test {
@@ -14,19 +15,20 @@ public class Test {
 //
 //        }
 
-        List<Employee> employees = EmployeeController.get();
-        for (Employee employee : employees) {
-            System.out.println("Id = "+employee.getId());
-            System.out.println("Name = "+employee.getName());
-            System.out.println("Mobile = "+employee.getMobile());
-            System.out.println("Address = "+employee.getAddress());
-            System.out.println("Dssig = "+employee.getDesignation().getName());
-            System.out.println("EmpSts = "+employee.getEmployeeststus().getName());
-            System.out.println("Gender = "+employee.getGender().getName());
-
-        }
-
-
+//        List<Employee> employees = EmployeeController.get();
+//        for (Employee employee : employees) {
+//            System.out.println("Id = "+employee.getId());
+//            System.out.println("Name = "+employee.getName());
+//            System.out.println("Mobile = "+employee.getMobile());
+//            System.out.println("Address = "+employee.getAddress());
+//            System.out.println("Dssig = "+employee.getDesignation().getName());
+//            System.out.println("EmpSts = "+employee.getEmployeeststus().getName());
+//            System.out.println("Gender = "+employee.getGender().getName());
+//
+//        }
+        EmployeeUi employeeUi = new EmployeeUi();
+        employeeUi.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        employeeUi.setVisible(true);
     }
 
 }
